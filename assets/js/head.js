b3bad8dbce09fd64d00947bbfd3e7ca78ae76f60
MinoTsai <hd1224634032@gmail.com>
2022-05-20 04:04:51 +0800

    upload

---
+++ assets/js/head.js
@@ -0,0 +1,17 @@
+head ='<title>Conference Room</title>\
+<!-- boostrap5 -->\
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0/dist/css/bootstrap.min.css" rel="stylesheet"\
+    integrity="sha384-wEmeIV1mKuiNpC+IOBjI7aAzPcEZeedi5yW5f2yOq55WWLwNGmvvx4Um1vskeMj0" crossorigin="anonymous">\
+\
+<!-- Google icon -->\
+<link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons">\
+<link href="https://fonts.googleapis.com/css?family=Material+Icons|Material+Icons+Outlined|Material+Icons+Two+Tone|Material+Icons+Round|Material+Icons+Sharp" rel="stylesheet">\
+\
+<link rel="stylesheet" href="assets/css/reset.css">\
+<link rel="stylesheet" href="assets/css/style.css?=<?php echo time(); ?>">\
+\
+<!--引用jQuery-->\
+<script src="https://code.jquery.com/jquery-3.5.1.js"></script>\
+<!-- boostrap5 -->\
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-p34f1UUtsS3wqzfto5wAAmdvj+osOnFyQFpp4Ua3gs/ZVWx6oOypYoCJhGGScy+8" crossorigin="anonymous"></script>';
+document.write(head);
