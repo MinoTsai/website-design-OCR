0c0360f8686a72a3c749aa57bc9280c7a60c63bf
MinoTsai <hd1224634032@gmail.com>
2022-05-25 10:52:49 +0800

    edit

--- assets/js/script.js
+++ assets/js/script.js
@@ -46,6 +46,7 @@ function copyEvent(id) {
     document.execCommand("Copy")
 }
 
+
 link ='<title>Conference Room</title>\
 <!-- boostrap5 -->\
 <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0/dist/css/bootstrap.min.css" rel="stylesheet"\
